47198460558295a33ddff93dc291b26ce8bc054a
tjrwls0826 <tjrwls0826@naver.com>
2022-05-23 19:12:53 +0900

    Update index.tsx

--- alecture/layouts/Workspace/index.tsx
+++ alecture/layouts/Workspace/index.tsx
@@ -153,7 +153,7 @@ const Workspace: VFC = () => {
             {showUserMenu && (
               <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onCloseUserProfile}>
                 <ProfileModal>
-                  <img src={gravatar.url(userData.nickname, { s: '36px', d: 'retro' })} alt={userData.nickname} />
+                  <img src={gravatar.url(userData.email, { s: '36px', d: 'retro' })} alt={userData.nickname} />
                   <div>
                     <span id="profile-name">{userData.nickname}</span>
                     <span id="profile-active">Active</span>
