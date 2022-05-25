20bdd87e89160d948471579c40189157c8171abe
tjrwls0826 <tjrwls0826@naver.com>
2022-05-25 20:50:09 +0900

    aNest First Commit

--- alecture/layouts/Workspace/index.tsx
+++ alecture/layouts/Workspace/index.tsx
@@ -148,7 +148,7 @@ const Workspace: VFC = () => {
     <div>
       <Header>
         <RightMenu>
-          <span onClick={onClickUserProfile}> //그라바타 이용 랜덤 프로필 사진
+          <span onClick={onClickUserProfile}>
             <ProfileImg src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname} />
             {showUserMenu && (
               <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onCloseUserProfile}>
@@ -177,11 +177,11 @@ const Workspace: VFC = () => {
           <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
         </Workspaces>
         <Channels>
-          <WorkspaceName onClick={toggleWorkspaceModal}>Sleact</WorkspaceName>
+          <WorkspaceName onClick={toggleWorkspaceModal}>나비게이션</WorkspaceName>
           <MenuScroll>
             <Menu show={showWorkspaceModal} onCloseModal={toggleWorkspaceModal} style={{ top: 95, left: 80 }}>
               <WorkspaceModal>
-                <h2>Sleact</h2>
+                <h2>나비게이션</h2>
                 <button onClick={onClickInviteWorkspace}>워크스페이스에 사용자 초대하기</button>
                 <button onClick={onClickAddChannel}>채널 만들기</button>
                 <button onClick={onLogout}>로그아웃</button>
